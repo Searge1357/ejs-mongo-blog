341dd165cb784d1fde245af0a3d186d4352588a5
Sergio <a00825769@itesm.mx>
2022-02-03 01:51:47 -0600

    Lab 9 completo

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,18 +1,50 @@
 
 const express = require('express');
 const router = express.Router();
-//const Task = require('../model/task');
+const Post = require('../model/post'); //Aquí declaramos el objeto
 
 
 router.get('/', async function(req,res){
-  res.render('index');
-});
+  let posts = await Post.find()
+  console.log(posts)
 
+  res.render('index',{posts});
+});
 
 router.get('/newPost', async (req,res) =>{
   res.render('newPost');
 });
 
+router.post('/newPost', async (req,res)=>{
+  let post = new Post(req.body)
+  //console.log(post)
+  await post.save()
+  res.redirect("/")
+})
 
+router.get('/edit/:id', async (req,res)=>{
+  let id = req.params.id 
+  let post = await Post.findById(id)
+  res.render('edit', {post})
+})
 
-module.exports = router;
+router.post('/edit/:id', async (req,res)=>{
+  await Post.updateOne({_id:req.params.id}, req.body)
+  //le mandamos el id, y los nuevos datos que tendrá el objeto de ese id
+  res.redirect("/")
+})
+
+router.get('/delete/:id', async (req,res)=>{
+  let id = req.params.id
+  let post = await Post.findById(id)
+
+  res.render('delete',{post})
+})
+
+router.post('/delete/:id', async (req,res)=>{
+  await Post.remove({_id:req.params.id})
+
+  res.redirect("/")
+})
+
+module.exports = router; //Esto lo exporta al archivo app.js
